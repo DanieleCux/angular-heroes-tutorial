dea25ac6de8f13591b5d02c5a81dd16df0675bef
Daniele Cuccovillo <dcuccovi@hotmail.com>
2018-11-08 17:40:58 +0100

    - adds and configures the Angular router - defines simple routes, a redirect route, and a parameterized route - turns the AppComponent into a navigation shell - uses the routerLink directive in anchor elements - refactors a tightly-coupled mastr/detl. view into a routed detail view - shares the HeroService among multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 // Modulo necessario per il 'two way data binding'; NB: va aggiunto anche più sotto, tra gli imports: []
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
