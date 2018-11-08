c3d3cd53c9e96d7e76903ce03198ff8abddeadd9
Daniele Cuccovillo <dcuccovi@hotmail.com>
2018-11-08 12:15:54 +0100

    - uses *ngFor to display a list - uses *ngIf to conditionally include or exclude a block of HTML - toggles a CSS style class with a class binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Modulo necessario per il 'two way data binding'; NB: va aggiunto anche più sotto, tra gli imports: []
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
